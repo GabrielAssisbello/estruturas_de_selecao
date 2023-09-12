a7b83bec7dc314ffd0d8fb58fa3a2c180d08590e
gabriel assis <gassis574@gmail.com>
2023-09-11 21:07:31 -0300

    ap03(selecao): fim do exemplo if

--- exemploIf.java
+++ exemploIf.java
@@ -1,1 +1,18 @@
-public 
+import javax.swing.JOptionPane;
+
+public class ExemploIf {
+    static public void main (String [] args){
+
+    double nota = 
+      Double.parseDouble(JOptionPane.showInputDialog
+      ("digita ai"));        
+
+     if(nota>=70){
+        JOptionPane.showMessageDialog(null, "aprovado");   
+     }
+
+
+
+    }
+
+}
